0a41145d7011690249cfde74f4a73502133f28cd
Tejas C R <tejascrtejascr898@gmail.com>
2026-09-02 23:50:48 +0530

    Day 28 - Added NumPy array indexing

---
+++ numpy_array.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40, 50])
+
+print("Array:", numbers)
+print("First number:", numbers[0])
+print("Third number:", numbers[2])
+print("Last number:", numbers[-1])
