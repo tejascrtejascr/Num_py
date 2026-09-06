450a4069e2181432dabf81bedd68a71a5b5e8732
Tejas C R <tejascrtejascr898@gmail.com>
2026-09-06 23:37:22 +0530

    Day 34 - Added NumPy statistics

---
+++ numpy_statistics.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40, 50])
+
+print("Mean:", np.mean(numbers))
+print("Median:", np.median(numbers))
+print("Standard deviation:", np.std(numbers))
