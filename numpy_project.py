c65ddea6366686934a391a793e42f63f43f60f99
Tejas C R <tejascrtejascr898@gmail.com>
2026-09-07 22:22:37 +0530

    Day 36 - Added NumPy mini project

---
+++ numpy_project.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+marks = np.array([75, 80, 65, 90, 85])
+
+print("Student marks:", marks)
+print("Total marks:", np.sum(marks))
+print("Average marks:", np.mean(marks))
+print("Highest marks:", np.max(marks))
+print("Lowest marks:", np.min(marks))
